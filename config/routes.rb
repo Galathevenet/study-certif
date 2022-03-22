caa866b83bcecaaa748352363e97ae743a8825d2
Gala Thevenet <galathevenet@hotmail.com>
2022-03-22 15:42:15 +0100

    routes created for flats and rentings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'flats#index'
+  resources :flats do
+    resources :rentings, only: [:create]
+  end
+  resources :rentings, only: [:index, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
