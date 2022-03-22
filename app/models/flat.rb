c141ee629f01cf8d3d604c7f2e5b84795202eda2
Gala Thevenet <galathevenet@hotmail.com>
2022-03-22 15:28:21 +0100

    validations and associations done for models

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,3 +1,7 @@
 class Flat < ApplicationRecord
   belongs_to :user
+  has_many :rentings, dependent: :destroy
+
+  validates :address, presence: true, uniqueness: true
+  validates :price, presence: true
 end
--- app/models/renting.rb
+++ app/models/renting.rb
@@ -1,4 +1,7 @@
 class Renting < ApplicationRecord
   belongs_to :user
   belongs_to :flat
+
+  STATUS = ["Accepted", "Pending", "Rejected"]
+  validates :status, inclusion: { in: STATUS }
 end
